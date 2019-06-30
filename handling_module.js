e5dd42cb5a488352a5ffbb787222cdca91198cb6
Erik Poole <Mahkumazahn@gmail.com>
2019-06-30 17:46:02 -0600

    Formatting

--- handling_module.js
+++ handling_module.js
@@ -1,54 +1,53 @@
-const fs = require('fs');
+const fs = require("fs");
 
 const readJSONFromFile = function(filename) {
-    let data = fs.readFileSync(filename);
-    return JSON.parse(data.toString());
+  let data = fs.readFileSync(filename);
+  return JSON.parse(data.toString());
 };
 
-
 exports.handleUpdate = function(filename, res) {
-    let request = filename.split("/update/")[1];
-    let name = request.split("=")[0];
-    let count = request.split("=")[1];
+  let request = filename.split("/update/")[1];
+  let name = request.split("=")[0];
+  let count = request.split("=")[1];
 
-    let parameters = readJSONFromFile("./data.json");
-    parameters[name] = count;
+  let parameters = readJSONFromFile("./data.json");
+  parameters[name] = count;
 
-    fs.writeFileSync("./data.json", JSON.stringify(parameters), function (err) {
-        if (err) {
-            throw err;
-        }
-    });
+  fs.writeFileSync("./data.json", JSON.stringify(parameters), function(err) {
+    if (err) {
+      throw err;
+    }
+  });
 
-    console.log("Received: " + request);
-    res.writeHead(200);
-    res.end();
+  console.log("Received: " + request);
+  res.writeHead(200);
+  res.end();
 };
 
 exports.handleInitialRequest = function(filename, res) {
-    let request = filename.split("/request/")[1];
+  let request = filename.split("/request/")[1];
 
-    let returnValue = "ERROR";
-    let parameters = readJSONFromFile("./data.json");
-    returnValue = parameters[request];
+  let returnValue = "ERROR";
+  let parameters = readJSONFromFile("./data.json");
+  returnValue = parameters[request];
 
-    console.log("Sent value for: " + request);
-    res.writeHead(200);
-    res.write(returnValue.toString());
-    res.end();
+  console.log("Sent value for: " + request);
+  res.writeHead(200);
+  res.write(returnValue.toString());
+  res.end();
 };
 
 exports.handleFileRequest = function(filename, res) {
-    fs.readFile(filename, function (err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            console.log("Failed");
-            res.end("404 Not Found")
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            console.log("Sent: " + filename);
-            res.end();
-        }
-    });
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      console.log("Failed");
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      console.log("Sent: " + filename);
+      res.end();
+    }
+  });
 };
--- health_counter.js
+++ health_counter.js
@@ -3,101 +3,99 @@ let tyrantDefaultValue = [0];
 let expDefaultValue = [0];
 
 function sendUpdate(counterString, count) {
-    let newValue = counterString + '=' + count;
-    let request = "http://" + location.host + "/update/" + newValue;
-    console.log("Sending Request: " + request);
-    let xml = new XMLHttpRequest();
-    xml.open("GET", request);
-    xml.send();
+  let newValue = counterString + "=" + count;
+  let request = "http://" + location.host + "/update/" + newValue;
+  console.log("Sending Request: " + request);
+  let xml = new XMLHttpRequest();
+  xml.open("GET", request);
+  xml.send();
 }
 
 const updateCounter = function(requestString, counterString, commandString) {
-    let countObj = document.getElementById(counterString);
-    let count = parseInt(countObj.innerText);
-    if (commandString === "up") {
-        count++;
-    } else if (commandString === "down") {
-        count--;
-    } else if (commandString === "reset") {
-        if (counterString === "bearCounter") {
-            count = bearDefaultValue[0];
-        } else if (counterString === "tyrantCounter") {
-            count = tyrantDefaultValue[0];
-        } else if (counterString === "expCounter") {
-            count = expDefaultValue[0];
-        }
-    } else {
-        throw "improper commandString"
+  let countObj = document.getElementById(counterString);
+  let count = parseInt(countObj.innerText);
+  if (commandString === "up") {
+    count++;
+  } else if (commandString === "down") {
+    count--;
+  } else if (commandString === "reset") {
+    if (counterString === "bearCounter") {
+      count = bearDefaultValue[0];
+    } else if (counterString === "tyrantCounter") {
+      count = tyrantDefaultValue[0];
+    } else if (counterString === "expCounter") {
+      count = expDefaultValue[0];
     }
+  } else {
+    throw "improper commandString";
+  }
 
-    countObj.innerText = count.toString();
-    sendUpdate(requestString, count);
+  countObj.innerText = count.toString();
+  sendUpdate(requestString, count);
 };
 
-
-const requestServerValue = function(requestString, returnLocation, handlerFunction) {
-    let request = "http://" + location.host + "/request/" + requestString;
-    console.log(request);
-    let xml = new XMLHttpRequest();
-    xml.open("GET", request);
-    xml.responseType = "text";
-    xml.onload = function() {
-        handlerFunction(returnLocation, xml);
-    };
-    xml.send();
+const requestServerValue = function(
+  requestString,
+  returnLocation,
+  handlerFunction
+) {
+  let request = "http://" + location.host + "/request/" + requestString;
+  console.log(request);
+  let xml = new XMLHttpRequest();
+  xml.open("GET", request);
+  xml.responseType = "text";
+  xml.onload = function() {
+    handlerFunction(returnLocation, xml);
+  };
+  xml.send();
 };
 
 const initialValueHandler = function(counterString, xmlObj) {
-    console.log("Received Initial Value for: " + counterString);
-    document.getElementById(counterString).innerText = xmlObj.response;
+  console.log("Received Initial Value for: " + counterString);
+  document.getElementById(counterString).innerText = xmlObj.response;
 };
 
 const defaultValueHandler = function(intBuffer, xmlObj) {
-    console.log("Recieved Default Value for: " + intBuffer);
-    intBuffer[0] = xmlObj.response;
+  console.log("Recieved Default Value for: " + intBuffer);
+  intBuffer[0] = xmlObj.response;
 };
-
 
 window.onload = function() {
-    document.getElementById("bearUp").onclick = function () {
-        updateCounter("bearCurrent","bearCounter", "up");
-    };
-    document.getElementById("bearDown").onclick = function () {
-        updateCounter("bearCurrent", "bearCounter", "down");
-    };
-    document.getElementById("bearReset").onclick = function () {
-        updateCounter("bearCurrent", "bearCounter", "reset");
-    };
+  document.getElementById("bearUp").onclick = function() {
+    updateCounter("bearCurrent", "bearCounter", "up");
+  };
+  document.getElementById("bearDown").onclick = function() {
+    updateCounter("bearCurrent", "bearCounter", "down");
+  };
+  document.getElementById("bearReset").onclick = function() {
+    updateCounter("bearCurrent", "bearCounter", "reset");
+  };
 
-    document.getElementById("tyrantUp").onclick = function () {
-        updateCounter("tyrantCurrent","tyrantCounter", "up");
-    };
-    document.getElementById("tyrantDown").onclick = function () {
-        updateCounter("tyrantCurrent","tyrantCounter", "down");
-    };
-    document.getElementById("tyrantReset").onclick = function () {
-        updateCounter("tyrantCurrent", "tyrantCounter", "reset");
-    };
+  document.getElementById("tyrantUp").onclick = function() {
+    updateCounter("tyrantCurrent", "tyrantCounter", "up");
+  };
+  document.getElementById("tyrantDown").onclick = function() {
+    updateCounter("tyrantCurrent", "tyrantCounter", "down");
+  };
+  document.getElementById("tyrantReset").onclick = function() {
+    updateCounter("tyrantCurrent", "tyrantCounter", "reset");
+  };
 
-    document.getElementById("expUp").onclick = function () {
-        updateCounter("expCurrent","expCounter", "up");
-    };
-    document.getElementById("expDown").onclick = function () {
-        updateCounter("expCurrent","expCounter", "down");
-    };
-    document.getElementById("expReset").onclick = function () {
-        updateCounter("expCurrent", "expCounter", "reset");
-    };
+  document.getElementById("expUp").onclick = function() {
+    updateCounter("expCurrent", "expCounter", "up");
+  };
+  document.getElementById("expDown").onclick = function() {
+    updateCounter("expCurrent", "expCounter", "down");
+  };
+  document.getElementById("expReset").onclick = function() {
+    updateCounter("expCurrent", "expCounter", "reset");
+  };
 
+  requestServerValue("bearDefault", bearDefaultValue, defaultValueHandler);
+  requestServerValue("tyrantDefault", tyrantDefaultValue, defaultValueHandler);
+  requestServerValue("expDefault", expDefaultValue, defaultValueHandler);
 
-    requestServerValue("bearDefault", bearDefaultValue, defaultValueHandler);
-    requestServerValue("tyrantDefault", tyrantDefaultValue, defaultValueHandler);
-    requestServerValue("expDefault", expDefaultValue, defaultValueHandler);
-
-    requestServerValue("bearCurrent", "bearCounter", initialValueHandler);
-    requestServerValue("tyrantCurrent", "tyrantCounter", initialValueHandler);
-    requestServerValue("expCurrent", "expCounter", initialValueHandler);
-
-
+  requestServerValue("bearCurrent", "bearCounter", initialValueHandler);
+  requestServerValue("tyrantCurrent", "tyrantCounter", initialValueHandler);
+  requestServerValue("expCurrent", "expCounter", initialValueHandler);
 };
-
